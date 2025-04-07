6029d089462b04003639d32d65093d7ddf9bc6c5
Youssef97dev <youssefmoussafi97@gmail.com>
2025-04-07 11:19:35 +0100

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Le Salama",
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics />
       <body>{children}</body>
     </html>
   );

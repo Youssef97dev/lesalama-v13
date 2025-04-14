8f91d41be66bacd162a9d890873fc13cb6453b36
Youssef97dev <youssefmoussafi97@gmail.com>
2025-04-14 13:39:38 +0100

    delete loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -16,12 +16,11 @@ const Loader = () => {
 
   useEffect(() => {
     // Simulate loading time
-    const timer = setTimeout(() => setIsClient(true), 2500);
-    return () => clearTimeout(timer);
+    setIsClient(true);
   }, []);
   return (
     <>
-      {isClient ? (
+      {isClient && (
         <div className="relative flex flex-col justify-center items-center">
           <Navbar />
           <Hero />
@@ -34,23 +33,6 @@ const Loader = () => {
           <Contact />
           <div className="fixed bottom-0 left-0 w-full block lg:hidden z-10">
             <BookingButton />
-          </div>
-        </div>
-      ) : (
-        <div className="fixed inset-0 z-40 flex items-center justify-center">
-          {/* Initial white background animation */}
-          <div className="absolute inset-0 flex z-50">
-            <div className="bg-color_1  flex-1 animate-slide-left"></div>
-            <div className="bg-color_1  flex-1 animate-slide-right"></div>
-          </div>
-
-          {/* Beige background with logo animation */}
-          <div className="absolute inset-0 bg-color_2 flex items-center justify-center">
-            <img
-              src="/logo-salama-humberger-white.png" // Replace with your logo path
-              alt="Logo"
-              className="w-32 h-32 animate-scale-3d"
-            />
           </div>
         </div>
       )}
